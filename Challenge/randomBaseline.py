259dbb2ae4c6242f71e6c4b93ab5be73a9fc7f2b
William Souza <willdoliver@Williams-MacBook-Pro.local>
2021-11-22 11:48:05 -0300

    Challenge: Algoritmo de redes neurais

---
+++ Challenge/randomBaseline.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+dfEdgesToEvaluate = pd.read_csv('edgesToEvaluate.csv')
+
+random_predictions = np.random.choice([0, 1], size=dfEdgesToEvaluate.shape[0])
+
+dfEdgesToEvaluate['link'] = random_predictions
+
+# Please pay attention: Kaggle demands that you add "linkID" and "link" column headers
+dfEdgesToEvaluate.to_csv("randomTeste.csv", columns=['linkID','link'],index=False)  
